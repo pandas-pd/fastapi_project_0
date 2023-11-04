6cf3d0426165c46b141ee8a7ab61491ab605d5dc
tausj1 <joel.tauss@students.bfh.ch>
2023-11-05 00:08:43 +0100

    added new columns

--- app/db/models/skills.py
+++ app/db/models/skills.py
@@ -1,21 +1,32 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from db.base import Base
 
 class Programming_languages(Base):
 
     __tablename__ = "skills.programming_languages_pl"
 
-    id_pl = Column(Integer, primary_key=True, index=True)
+    #columns
+    id_pl           = Column(Integer, primary_key = True, index = True)
+    fk_sl           = Column(Integer, ForeignKey("enum.skill_level_sl.id_sl", onupdate = "CASCADE"))
 
-    name = Column(String, index=True, nullable=False)
-    description = Column(String, nullable=True)
+    name            = Column(String, index = True, nullable = False)
+    comment         = Column(String, nullable = True)
+
+    #relations:
+    children        = relationship("Libraries", backref = "parent")
+
 
 class Libraries(Base):
 
     __tablename__ = "skills.libraries_lb"
 
-    id_lb = Column(Integer, primary_key=True, index=True)
-    fk_pl = Column(Integer, ForeignKey("skills.programming_languages_pl.id_pl"))
+    #columns
+    id_lb           = Column(Integer, primary_key = True, index = True)
+    fk_pl           = Column(Integer, ForeignKey("skills.programming_languages_pl.id_pl", onupdate = "CASCADE"))
+    fk_sl           = Column(Integer, ForeignKey("enum.skill_level_sl.id_sl", onupdate = "CASCADE"))
 
-    name = Column(String, index=True, nullable=False)
-    description = Column(String, nullable=True)
+    name            = Column(String, index = True, nullable = False)
+    description     = Column(String, nullable = True)
+
+    #relations:
