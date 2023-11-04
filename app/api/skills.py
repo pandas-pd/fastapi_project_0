0c88c78073345e4818c7f793450e078f43279660
tausj1 <joel.tauss@students.bfh.ch>
2023-11-05 00:08:01 +0100

    added new column: skill_level

--- app/api/skills.py
+++ app/api/skills.py
@@ -1,15 +1,14 @@
 from fastapi import APIRouter
-from sqlalchemy.orm import Session
-#from db.models.skills import Item
-from db.base import session
+from services.skills import *
 
 class Endpoint():
 
     router = APIRouter()
 
-    @router.get("/get_skills")
-    def get_all_skills():
+    @router.get("/get_programming_languages")
+    def get_programming_languages():
 
-        return {"skill1": "hello from file"}
+        content = Read.all_programming_languages()
+        return content
 
 
