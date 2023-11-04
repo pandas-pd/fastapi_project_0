0af4f3bd242078914dfc076eac7de7a8ebcb96cb
tausj1 <joel.tauss@students.bfh.ch>
2023-11-05 00:08:19 +0100

    in dev

---
+++ app/db/models/enums.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from db.base import Base
+
+class Skill_level(Base):
+
+    __tablename__ = "enum.skill_level_sl"
+
+    #columns
+    id_sl       = Column(Integer, primary_key = True, index = True)
+
+    code         = Column(Integer, nullable = False)
+    value        = Column(String(50), nullable = True)
+
+    #relations
+    children        = relationship("Programming_languages", backref = "parent")
+    children        = relationship("Libraries", backref = "parent")
