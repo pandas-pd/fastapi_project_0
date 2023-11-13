8c212719cc3e8305d7b2e2c1e9c51b3c3d4ac9a7
tausj1 <joel.tauss@students.bfh.ch>
2023-11-13 16:06:31 +0100

    in dev: get_all_lang

--- app/services/skills.py
+++ app/services/skills.py
@@ -1,5 +1,7 @@
 from db.base import session
+from sqlalchemy import select, insert, delete, update
 from db.models.skills import Programming_languages, Libraries
+from db.models.enums import Skill_level
 
 class Write():
     pass
@@ -10,10 +12,28 @@ class Read():
         """reads all skill from the corresponing model withoug filters"""
 
         column_names = Programming_languages.__table__.columns.keys()
-        print(type(column_names))
 
-        return {"content" : f"{column_names}"}
-        #session
+        query = select(
+            Programming_languages.name,
+            Programming_languages.comment,
+            Programming_languages.timestamp,
+            Skill_level.code,
+            Skill_level.code,
+        ).join_from(
+            Programming_languages,
+            Skill_level,
+        )
+
+        content = session.execute(query).fetchall()
+
+        #to be formated
+        response = {
+            "cols" : column_names,
+            "content" : content,
+        }
+
+        return response
+
 
 class Update():
     pass
