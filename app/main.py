c4785730870821935678e4d3add3d973e729eb25
tausj1 <joel.tauss@students.bfh.ch>
2023-11-13 16:05:38 +0100

    added import and enums

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
-from api import skills  # Import your route modules
+from api import skills, enums  # Import your route modules
 from db.base import engine
 from db import base
+import os
+
+#make all moduls accessable form everywhere
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 #docu can be found under /docs#/default/
-
 #load description
 file = open("docu.md", "r")
 description = file.read()
@@ -19,6 +23,7 @@ app = FastAPI(
 #include routes, add news to add to funcitonality
 routes : list =[
     skills.Endpoint.router,
+    enums.Endpoint.router,
     #add new here
 ]
 
