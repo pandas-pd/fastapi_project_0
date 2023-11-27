359d02685cef493fa788e01400bec0afec975780
tausj1 <joel.tauss@students.bfh.ch>
2023-11-27 22:46:40 +0100

    note to self, add project status fetcher

--- app/api/enums.py
+++ app/api/enums.py
@@ -9,5 +9,10 @@ class Endpoint():
     def get_skill_levels():
 
         response = Read.skill_level()
+        return response
 
+    @router.get("/get_project_status", tags = ["enum"])
+    def get_project_status():
+
+        response = {"message" : "implement this you rice cracker"}
         return response
