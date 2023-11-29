62b0d0f70590a2e02cca0a97905dc9c8d825a1f5
tausj1 <joel.tauss@students.bfh.ch>
2023-11-29 12:47:19 +0100

    rewrote to accomadate with setup.py changes

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,12 @@
+import os
+
+#pathing
+db_url : str                    = "sqlite.db"
+settings_dir : str              = os.path.dirname(os.path.abspath(__file__))
+DATABASE_URL : str              = "sqlite:///" + os.path.join(settings_dir, db_url)
+
 #database file, add as dynamic path to minimize seutp errors
-DATABASE_URL = "sqlite:///sqlite.db"
+#DATABASE_URL = "sqlite:///sqlite.db"
 
 #other settings
 TIME_ZONE = None #not in use
