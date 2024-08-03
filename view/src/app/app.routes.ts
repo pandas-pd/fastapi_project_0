d0969b8aa939e0ca7f690a12588bc04dcbd364ec
teapot418 <joel.tauss@gmail.com>
2024-08-03 21:53:50 +0200

    added placeholder for each menu

--- view/src/app/app.routes.ts
+++ view/src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
+
 import { HomeComponent } from './components/home/home.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { UsersComponent } from './components/users/users.component';
+import { LoginComponent } from './components/login/login.component';
+
+
 
 export const routes: Routes = [
 
     {
-        path: 'home',
+        path: '',
         component: HomeComponent,
     },
 
@@ -14,6 +20,27 @@ export const routes: Routes = [
         loadComponent: () =>
             import('./components/skills/skills.component').then((c) => c.SkillsComponent),
         //component: SkillsComponent,
-    }
+    },
+
+    {
+        path: 'projects',
+        loadComponent: () =>
+            import('./components/projects/projects.component').then((c) => c.ProjectsComponent),
+        //component: SkillsComponent,
+    },
+
+    {
+        path: 'users',
+        loadComponent: () =>
+            import('./components/users/users.component').then((c) => c.UsersComponent),
+        //component: SkillsComponent,
+    },
+
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./components/login/login.component').then((c) => c.LoginComponent),
+        //component: SkillsComponent,
+    },
 
 ];
