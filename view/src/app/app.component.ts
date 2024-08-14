a8d2b88bb0be8900ca68992ca51690625062647c
teapot418 <joel.tauss@gmail.com>
2024-08-14 19:28:40 +0200

    login form stuff

--- view/src/app/app.component.ts
+++ view/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 //components
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -21,6 +21,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'view';
+    title = 'view';
 }
