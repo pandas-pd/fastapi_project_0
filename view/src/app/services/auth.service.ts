3bb665e13f9d8fa05cbf988d23e3e436e98a585f
teapot418 <joel.tauss@gmail.com>
2024-10-23 15:23:51 +0200

    n

--- view/src/app/services/auth.service.ts
+++ view/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { environment } from '../../environments/environment';
 //import { getCookie } from '../utils/cookie';
 
 
