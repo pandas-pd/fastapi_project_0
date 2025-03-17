18d762a87c039a6ecad46ca28c18d4a6cfe9f178
teapot418 <joel.tauss@gmail.com>
2025-03-17 18:23:25 +0100

    smol changes, resolvers in dev

--- view/src/app/components/account/account.component.ts
+++ view/src/app/components/account/account.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ApiUsersService } from '../../services/api-users.service';
 
 @Component({
-  selector: 'app-account',
-  standalone: true,
-  imports: [],
-  templateUrl: './account.component.html',
-  styleUrl: './account.component.css'
+    selector: 'app-account',
+    standalone: true,
+    imports: [],
+    templateUrl: './account.component.html',
+    styleUrl: './account.component.css'
 })
+
 export class AccountComponent {
+
+    constructor(private apiUserService:ApiUsersService){}
 
 }
---
+++ view/src/app/resolvers/account-resolver.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AccountResolverService } from './account-resolver.service';
+
+describe('AccountResolverService', () => {
+  let service: AccountResolverService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AccountResolverService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
